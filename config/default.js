bb13c93e4183ae097384a30d3ac70a0ff16cb161
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2017-06-21 11:51:09 +0300

    change fit to limit

--- config/default.js
+++ config/default.js
@@ -39,9 +39,9 @@ const conf = {
     "batchSize": process.env.CLOUDINARY_BATCH || 50,
     "transformations": process.env.CLOUDINARY_TRANSF || [
       {quality: 'auto', crop: 'fit'},
-      {quality: 'auto', fetch_format: 'webp', crop: 'fit'},
-      {quality: 'auto', fetch_format: 'wdp', crop: 'fit'},
-      {quality: 'auto', fetch_format: 'png', crop: 'fit'}
+      {quality: 'auto', fetch_format: 'webp', crop: 'limit'},
+      {quality: 'auto', fetch_format: 'wdp', crop: 'limit'},
+      {quality: 'auto', fetch_format: 'png', crop: 'limit'}
     ]
   }
 };
