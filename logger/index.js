c50b4306b3d4576cc0eb20cbd542f5725fcb8efc
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2017-07-26 20:55:18 +0300

    logger changes

--- logger/index.js
+++ logger/index.js
@@ -1,14 +1,13 @@
+require('dotenv').config();
 const config = require('config');
 const Rollbar = require('rollbar');
 logger = new Rollbar({
   accessToken: config.get('rollbar.postToken'),
   handleUncaughtExceptions: true,
   handleUnhandledRejections: true,
-  payload: {
-    environment: process.env.NODE_ENV | 'N/A'
-  }
+  environment: process.env.NODE_ENV || process.env.HOSTNAME
 });
 if ('development' === process.env.NODE_ENV) {
-  logger.configure({  verbose: true});
+  logger.configure({verbose: true});
 }
 module.exports = logger;
