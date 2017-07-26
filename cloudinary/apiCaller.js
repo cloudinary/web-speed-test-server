cd3862b58c700b40b66b80021ca73abf215f39fc
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2017-07-26 10:32:29 +0300

    added server metadata

--- cloudinary/apiCaller.js
+++ cloudinary/apiCaller.js
@@ -10,49 +10,81 @@ const _ = require('lodash');
 const cloudinaryParser = require('./cloudinaryResultParser');
 const cloudinary = require('cloudinary');
 const async = require('async');
+const request = require('request');
 
 const sentToAnalyze = (imagesArray, dpr, metaData, cb) => {
     let batchSize = config.get('cloudinary.batchSize');
-    let analyzeResults = [];
-    let timestamp = Math.floor(Date.now() / 1000);
-    async.eachLimit(imagesArray, batchSize, (image, callback) => {
-      let context = {
-        rendered_dimensions: {width: image.width, height: image.height},
-        dpr: dpr,
-        request_headers: metaData.headers
-      };
-      let transformations = config.get('cloudinary.transformations', null);
-      if (typeof transformations === 'string') {
-        transformations = JSON.parse(transformations);
+    addServerInfo(imagesArray, batchSize, dpr, metaData, cb);
+};
+
+const addServerInfo = (imageList, batchSize, dpr, metaData, cb) => {
+  async.eachLimit(imageList, batchSize, (img, callback) => {
+    getServer(img, callback);
+  }, err => {
+    if (err) {
+      //log this
+    }
+    sendToCloudinery(imageList, batchSize, dpr, metaData, cb);
+  });
+};
+
+const sendToCloudinery = (imagesArray, batchSize, dpr, metaData, cb) => {
+  let analyzeResults = [];
+  let timestamp = Math.floor(Date.now() / 1000);
+  async.eachLimit(imagesArray, batchSize, (image, callback) => {
+    let context = {
+      rendered_dimensions: {width: image.width, height: image.height},
+      dpr: dpr,
+      request_headers: metaData.headers
+    };
+    let transformations = config.get('cloudinary.transformations', null);
+    if (typeof transformations === 'string') {
+      transformations = JSON.parse(transformations);
+    }
+    let eager = [];
+    if (transformations) {
+      eager = transformations.map((trans) => {
+        return Object.assign(trans, {width: image.width, height: image.height, dpr: dpr || 1});
+      });
+    }
+    //let server = getServer(image.url);
+    cloudinary.v2.uploader.upload(image.url, {eager: eager, analyze:{context: context}, tags: timestamp},(error, result) => {
+      if (error) {
+        analyzeResults.push({public_id: null});
+        logger.error('Error uploading to cloudinary', error);
+        callback();
+      } else {
+        result.server = image.server;
+        analyzeResults.push(result);
+        callback();
       }
-      let eager = [];
-      if (transformations) {
-        eager = transformations.map((trans) => {
-          return Object.assign(trans, {width: image.width, height: image.height, dpr: dpr || 1});
-        });
-      }
-      cloudinary.v2.uploader.upload(image.url, {eager: eager, analyze:{context: context}, tags: timestamp},(error, result) => {
-        if (error) {
-          analyzeResults.push({public_id: null});
-          logger.error('Error uploading to cloudinary', error);
-          callback();
-        } else {
-          analyzeResults.push(result);
-          callback();
-        }
-      } );
-    }, err => {
-      if (err) {
-        cb({status: 'error', message: 'Error getting results from cloudinary', error: err.message}, null);
-      }
-      let parsed = cloudinaryParser.parseCloudinaryResults(analyzeResults);
-      if (parsed.status === 'error') {
-        cb(parsed, null);
-        return;
-      }
-      Object.assign(parsed.resultSumm, metaData);
-      cb(null, {status: 'success', data : parsed });
-    })
+    } );
+  }, err => {
+    if (err) {
+      cb({status: 'error', message: 'Error getting results from cloudinary', error: err.message}, null);
+    }
+    let parsed = cloudinaryParser.parseCloudinaryResults(analyzeResults);
+    if (parsed.status === 'error') {
+      cb(parsed, null);
+      return;
+    }
+    Object.assign(parsed.resultSumm, metaData);
+    cb(null, {status: 'success', data : parsed });
+  })
+
+};
+
+const getServer = (image, callback) => {
+  request.head(image.url, (error, response, body) => {
+    if (error) {
+      //log this
+      callback();
+    } else {
+      image.server = (response.headers.server) ? response.headers.server : 'N/A';
+      callback();
+    }
+  });
+
 };
 
 module.exports = sentToAnalyze;
