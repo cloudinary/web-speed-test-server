fac564b6a0151f8a60e9a77e67b00cd99b025dad
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2018-04-02 13:13:54 +0300

    report level

--- logger/index.js
+++ logger/index.js
@@ -6,7 +6,7 @@ const logger = new Rollbar({
   handleUncaughtExceptions: true,
   handleUnhandledRejections: true,
   environment: process.env.NODE_ENV || process.env.HOSTNAME,
-  logLevel: process.env.LOG_LEVEL || 'info'
+  reportLevel: process.env.LOG_LEVEL || 'info'
 });
 if ('development' === process.env.NODE_ENV) {
   logger.configure({verbose: true});
