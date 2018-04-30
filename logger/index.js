5d2a69100106c088a42f967a2a958f6ef16067ce
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2018-04-30 13:59:30 +0300

    added system info to log

--- logger/index.js
+++ logger/index.js
@@ -4,12 +4,22 @@ const Rollbar = require('rollbar');
 const LOG_LEVEL_INFO = 'info';
 const LOG_LEVEL_WARNING = 'warning';
 const LOG_LEVEL_ERROR = 'error';
+const packageJson = require('../package.json');
+const os = require('os');
 const logger = new Rollbar({
   accessToken: config.get('rollbar.postToken'),
   handleUncaughtExceptions: true,
   handleUnhandledRejections: true,
   environment: process.env.NODE_ENV || process.env.HOSTNAME,
   reportLevel: process.env.LOG_LEVEL || 'info',
+  payload: {
+    system: {
+      appVersion: packageJson.version,
+      hostname: os.hostname(),
+      platform: os.platform(),
+      type: os.type(),
+    },
+  },
 });
 if ('development' === process.env.NODE_ENV) {
   //logger.configure({verbose: true});
@@ -19,5 +29,5 @@ module.exports = {
   logger: logger,
   LOG_LEVEL_INFO: LOG_LEVEL_INFO,
   LOG_LEVEL_WARNING: LOG_LEVEL_WARNING,
-  LOG_LEVEL_ERROR: LOG_LEVEL_ERROR
+  LOG_LEVEL_ERROR: LOG_LEVEL_ERROR,
 };
