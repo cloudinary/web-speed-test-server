6c26369f98e6e6d74104c9ec6279999987a93223
Yaron Tomer <yaron.tomer@cloudinary.com>
2022-03-23 18:22:01 +0200

    update lodash version

--- logger/index.js
+++ logger/index.js
@@ -27,7 +27,7 @@ const logger = new Rollbar({
 });
 
 if ('development' === process.env.NODE_ENV) {
-  // logger.configure({verbose: true});
+  logger.configure({verbose: true});
 }
 
 module.exports = {
