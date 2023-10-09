095dd8d033931792d8912ef7a0fa8e6710e8f394
Łukasz Czerpak <89903002+lukaszczerpak-cloudinary@users.noreply.github.com>
2023-10-09 16:06:14 +0200

    Delo 4068 cloudinary web speed test server package json remote code execution rce (#61)
    * updated deps, minor adjustments and improvements
    
    * paackage name change to avoid confusion (https://security.snyk.io/package/npm/pagespeed-server)

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   if ('OPTIONS' === req.method) {
     //respond with 200
-    res.send(200);
+    res.sendStatus(200);
   }
   else {
     //move on
